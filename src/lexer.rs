08521d68762c3f05788ec9a89d2afd9f4ae1f994
Facundo Venturi <venturifaku@gmail.com>
2023-10-18 16:05:14 -0300

    Iterator boilerplate

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,8 @@
-use std::io::Result;
+#![allow(dead_code)]
+use std::slice::Iter;
 
-enum TokenType {
+#[derive(PartialEq, Eq, Debug)]
+pub enum TokenType {
     LeftParentheses,
     RightParentheses,
     LeftBrace,
@@ -47,10 +49,11 @@ enum TokenType {
     Const,
     // Special tokens
     Error,
-    EOF,
+    // EOF,
 }
 
-enum Value {
+#[derive(PartialEq, Debug)]
+pub enum Value {
     None,
     Num(f64),
     Str(String),
@@ -58,12 +61,63 @@ enum Value {
     Name(String),
 }
 
+#[derive(PartialEq, Debug)]
 pub struct Token {
-    token_type: TokenType,
-    value: Value,
+    pub token_type: TokenType,
+    pub value: Value,
+    pub line: u32,
+}
+
+pub struct Scanner<'a> {
+    source_iter: Iter<'a, u8>,
+    token: u32,
     line: u32,
 }
 
-pub fn parse(source: &[u8]) -> Result<Vec<Token>> {
-    todo!();
+impl<'a> Scanner<'a> {
+    pub fn new(source: &'a [u8]) -> Self {
+        Scanner {
+            source_iter: source.iter(),
+            token: 1,
+            line: 1,
+        }
+    }
+}
+
+impl<'a> Iterator for Scanner<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.source_iter.next() {
+            Some(chr) => Some(Token {
+                token_type: TokenType::Error,
+                value: Value::Str(chr.to_string()),
+                line: self.line,
+            }),
+            None => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scanner_new() {
+        let source = b"5 + 4 - 1;";
+        let scanner = Scanner::new(source);
+        assert_eq!(scanner.token, 1);
+        assert_eq!(scanner.line, 1);
+    }
+
+    #[test]
+    fn test_scanner_next() {
+        let source = b"5 + 4 - 1;";
+        let mut scanner = Scanner::new(source);
+        let token = scanner.next().unwrap();
+        assert_eq!(token.token_type, TokenType::Number);
+        assert_eq!(token.value, Value::Num(5.0));
+        assert_eq!(token.line, 1);
+    }
 }
